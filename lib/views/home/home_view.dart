0f935fe8e9ecaa117c3a0dc16f86f6ffca2a7b90
kathya marquez <knmarquez25@gmail.com>
2021-03-19 00:23:00 -0700

    Compatibe with different window sizes

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:webapp/views/widgets/callToAction/callToAction.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:webapp/views/home/home_content_desktop.dart';
+import 'package:webapp/views/home/home_content_mobile.dart';
 import 'package:webapp/views/widgets/centeredView/centered_view.dart';
-import 'package:webapp/views/widgets/courseDetails/couse_details.dart';
-import 'package:webapp/views/widgets/navigationBar/nav_bar.dart';
+import 'package:webapp/views/widgets/navigationBar/navigation_bar.dart';
+import 'package:webapp/views/widgets/navigation_drawer/navigation_drawer.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CenteredView(
-        child: Column(
-          children: <Widget>[
-            NavigationBar(),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  CourseDetails(),
-                  Expanded(
-                    child: Center(
-                      child: callToaAction('Join Course'),
-                    ),
-                  )
-                ],
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? NavigationDrawer()
+            : null,
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                child: ScreenTypeLayout(
+                  mobile: HomeContentMobile(),
+                  desktop: HomeContentDesktop(),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -37,3 +38,5 @@ class HomeView extends StatelessWidget {
 //flutter build web
 //firebase deploy --only hosting
 //https://practicewebsite-e0e9e.web.app
+
+//github
